d3125fb385382c6f89a98fbfd8b50bad7646cb22
Mykola Marzhan <mykola.marzhan@percona.com>
2021-11-06 09:42:35 +0200

    avoid setting defaults in CR

--- pkg/controller/perconaservermongodb/psmdb_controller.go
+++ pkg/controller/perconaservermongodb/psmdb_controller.go
@@ -205,6 +205,11 @@ func (r *ReconcilePerconaServerMongoDB) Reconcile(request reconcile.Request) (re
 		return rr, err
 	}
 
+	if cr.ObjectMeta.DeletionTimestamp != nil {
+		err = r.checkFinalizers(cr)
+		return rr, err
+	}
+
 	clusterStatus := api.AppStateInit
 
 	defer func() {
@@ -228,11 +233,6 @@ func (r *ReconcilePerconaServerMongoDB) Reconcile(request reconcile.Request) (re
 	err = r.safeDownscale(cr)
 	if err != nil {
 		return reconcile.Result{}, errors.Wrap(err, "safe downscale")
-	}
-
-	if cr.ObjectMeta.DeletionTimestamp != nil {
-		err = r.checkFinalizers(cr)
-		return rr, err
 	}
 
 	err = r.reconcileUsersSecret(cr)
